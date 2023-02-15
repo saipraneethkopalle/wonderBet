3d09773d4459b022291a8b65169115017ed214ba
saipraneethkopalle <115973738+saipraneethkopalle@users.noreply.github.com>
2023-02-15 11:36:22 +0530

    changes done

--- frontEndBet/src/app/blockmarket/blockmarket.component.ts
+++ frontEndBet/src/app/blockmarket/blockmarket.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blockmarket.component.css']
 })
 export class BlockmarketComponent implements OnInit {
-
+  sportsData:any;
   constructor() { }
 
   ngOnInit(): void {
+    this.sportsData =[{betfairId:1,Name:"Soccer",status:"Soccer is ON"},{betfairId:2,Name:"Tennis",status:"Tennis is ON"},{betfairId:4,Name:"Cricket",status:"Cricket is ON"}]
   }
+
 
 }
