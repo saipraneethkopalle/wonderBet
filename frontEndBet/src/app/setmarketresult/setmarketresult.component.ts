228bd9f8b86c26850a74bd7d5a4834f75f45b103
SUBRAT <subrat.emineneceinnovation@gmail.com>
2023-02-16 12:12:08 +0530

    changes done

--- frontEndBet/src/app/setmarketresult/setmarketresult.component.ts
+++ frontEndBet/src/app/setmarketresult/setmarketresult.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as moment from 'moment';
 @Component({
   selector: 'app-setmarketresult',
   templateUrl: './setmarketresult.component.html',
   styleUrls: ['./setmarketresult.component.css']
 })
 export class SetmarketresultComponent implements OnInit {
-
+  betListData:any;
+  error:any =true;
+  currentDate:any=moment().format("YYYY-MM-DD hh:mm:ss");
+  // currentTime:any=moment().format("hh:mm");
   constructor() { }
 
   ngOnInit(): void {
+    // this.currentDate = "2023-02-25 03:54:18 PM"
+    console.log("current date",this.currentDate)
+  }
+  get(v:any){
+    console.log("clicked",v.target.value)
   }
 
 }
