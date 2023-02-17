c1b49a0df27037658bd666061f8cd7b89af7188a
saipraneethkopalle <115973738+saipraneethkopalle@users.noreply.github.com>
2023-02-17 15:31:35 +0530

    changes done

--- frontEndBet/src/app/login-page/login-page.component.ts
+++ frontEndBet/src/app/login-page/login-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiServicesService } from '../api-services.service';
 
@@ -14,10 +14,11 @@ export class LoginPageComponent implements OnInit {
   error:any;
   constructor(private router:Router,private apiService:ApiServicesService) { }
   register = new FormGroup({
-    userName:new FormControl(''),
-    password:new FormControl(''),
+    userName:new FormControl('',[Validators.required,Validators.minLength(18),Validators.pattern(/[a-zA-Z0-9]/)]),
+    password:new FormControl('',[Validators.required]),
     validationCode:new FormControl('')
   })
+  get f() { return this.register.controls; }
   ngOnInit(): void {
   this.generateValidationCode()
   }
