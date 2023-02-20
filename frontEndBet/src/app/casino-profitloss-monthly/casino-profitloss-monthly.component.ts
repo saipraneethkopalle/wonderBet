6f9124de7a77e1c2b25eddf4349bac6c34687f33
subhashinimayrya <subhashini20@navgurukul.org>
2023-02-20 03:13:14 -0800

    changes

--- frontEndBet/src/app/casino-profitloss-monthly/casino-profitloss-monthly.component.ts
+++ frontEndBet/src/app/casino-profitloss-monthly/casino-profitloss-monthly.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-casino-profitloss-monthly',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./casino-profitloss-monthly.component.css']
 })
 export class CasinoProfitlossMonthlyComponent implements OnInit {
+  currentDate:any=moment().format("YYYY-MM-DD");
+  currentTime:any=moment().format("hh:mm");
 
   constructor() { }
 
