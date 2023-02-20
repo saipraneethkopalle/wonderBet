39b59610868e11f32418ef1e24a92cc322fe0191
saipraneethkopalle <115973738+saipraneethkopalle@users.noreply.github.com>
2023-02-20 16:00:16 +0530

    changes done

--- frontEndBet/src/app/addmatch/addmatch.component.ts
+++ frontEndBet/src/app/addmatch/addmatch.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiServicesService } from '../api-services.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-addmatch',
@@ -6,13 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addmatch.component.css']
 })
 export class AddmatchComponent implements OnInit {
-
-  constructor() { }
+  cricketList:any;
+  soccerList:any;
+  tennisList:any;
+  matchesExist:any;
+  matchList: any;
+  constructor(private apiService:ApiServicesService) { }
 
   ngOnInit(): void {
-    
+    this.newMatches();
   }
+  newMatches(){
+    this.apiService.getMatches().subscribe((res:any)=>{
+      this.matchList = res.data;
+      this.cricketList = this.matchList.filter((sp:any)=>{if(sp.sportId=='4') return sp;})
+      this.tennisList = this.matchList.filter((sp:any)=>{if(sp.sportId=='1') return sp;})
+      this.soccerList = this.matchList.filter((sp:any)=>{if(sp.sportId=='2') return sp;})
+      this.getAllMatches();
+    })
 
-  
+  }
+  addMatch(data:any){
+    this.apiService.addMatches(data).subscribe((res:any)=>{
+      this.matchList = this.matchList.filter((ml:any)=>{
+        if(ml.eventId != data.eventId){
+          return ml;
+        }
+      })
+      this.cricketList = this.matchList.filter((sp:any)=>{if(sp.sportId=='4') return sp;})
+      this.tennisList = this.matchList.filter((sp:any)=>{if(sp.sportId=='1') return sp;})
+      this.soccerList = this.matchList.filter((sp:any)=>{if(sp.sportId=='2') return sp;})
+      Swal.fire({
+        title:'Success!',
+        text:`${data.eventName} Match Added Successfully`,
+        timer:1500
+
+      })
+    })
+  }
+  getAllMatches(){
+    this.apiService.getAllMatches().subscribe((res:any)=>{
+      this.matchesExist = res.data
+      this.matchList = this.matchList.filter((fs:any)=>{
+        if(this.matchesExist.filter((mE:any)=>{if(mE.eventId == fs.eventId) return mE;}).length == 0){
+          return fs;
+        }
+      })
+      this.cricketList = this.matchList.filter((sp:any)=>{if(sp.sportId=='4') return sp;})
+      this.tennisList = this.matchList.filter((sp:any)=>{if(sp.sportId=='1') return sp;})
+      this.soccerList = this.matchList.filter((sp:any)=>{if(sp.sportId=='2') return sp;})
+    })
+  }
 
 }
--- frontEndBet/src/app/sportsetting/sportsetting.component.ts
+++ frontEndBet/src/app/sportsetting/sportsetting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiServicesService } from '../api-services.service';
 
 @Component({
   selector: 'app-sportsetting',
@@ -8,13 +9,25 @@ import { Component, OnInit } from '@angular/core';
 export class SportsettingComponent implements OnInit {
   compo:any[]=["Cricket","Tennis","Soccer"]
   currentcompo:any
-  constructor() { }
+  matchList:any;
+  cricketList:any;
+  tennisList:any;
+  soccerList:any;
+  constructor(private apiService:ApiServicesService) { }
 
   ngOnInit(): void {
     this.findcompo(0)
+    this.getAllMatches();
   }
   findcompo(selectedcompo:any){
     this.currentcompo=selectedcompo
   }
-
+  getAllMatches(){
+    this.apiService.getAllMatches().subscribe((res:any)=>{
+      this.matchList = res.data
+      this.cricketList = this.matchList.filter((sp:any)=>{if(sp.sportId=='4') return sp;})
+      this.tennisList = this.matchList.filter((sp:any)=>{if(sp.sportId=='1') return sp;})
+      this.soccerList = this.matchList.filter((sp:any)=>{if(sp.sportId=='2') return sp;})
+    })
+  }
 }
