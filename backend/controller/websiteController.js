6a0a5be75fa5405346241f94ce28dc7d00ded53c
subhashinimayrya <subhashini20@navgurukul.org>
2023-02-23 21:49:17 -0800

    changes done

--- backend/controller/websiteController.js
+++ backend/controller/websiteController.js
@@ -1,14 +1,20 @@
 const { STATUS } = require("../constants/Config");
 const CONST = require("../constants/CONST");
 const cryp = require("../constants/cryptojs");
-const match = require("../models/match");
 const addWebsites=require("../models/websit");
 exports.addWebsite = async(req,res)=>{
     try{
         let data = await cryp.decryptData(req.body.payload)
         console.log(data);
+        let websiteData = await addWebsites.find({websiteName:data.websiteName}).lean();
+        if(websiteData.length > 0){
+            return res.status(STATUS.BAD_REQUEST).send({"error":"Already Exist"})
+        }
+        let addWebsite = new superUser(data);
+        addWebsite.save();
+        return res.status(STATUS.OK).send({"message":"Website Added  Successfully"})
        
     }catch(err){
-        console.log("error");
+        return res.status(STATUS.BAD_REQUEST).send(err);
     }
 }
