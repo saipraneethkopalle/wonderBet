6864ed18dff97dbe921de0efd1da75623f812d75
saipraneethkopalle <115973738+saipraneethkopalle@users.noreply.github.com>
2023-02-28 18:15:54 +0530

    changes done

--- frontEndBet/src/app/search-filter.pipe.ts
+++ frontEndBet/src/app/search-filter.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchFilterPipe implements PipeTransform {
 
   transform(list: any[], filterText: string): any {
-    return list ? list.filter(item => item.eventName.search(new RegExp(filterText, 'i')) > -1) : [];
+    return list ? list.filter(item => item.name?.search(new RegExp(filterText, 'i')) > -1) : [];
   }
 
 }
