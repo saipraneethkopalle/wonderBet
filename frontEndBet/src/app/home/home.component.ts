20d1fc0255f4fca1dc719cff10f995251561840e
SUBRAT <subrat.emineneceinnovation@gmail.com>
2023-03-03 17:36:16 +0530

    changes done

--- frontEndBet/src/app/home/home.component.ts
+++ frontEndBet/src/app/home/home.component.ts
@@ -21,6 +21,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAdmin();
+    
   }
   passwordForm = new FormGroup({
     password: new FormControl('',[Validators.required])
