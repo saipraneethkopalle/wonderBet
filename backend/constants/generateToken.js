745643e3d8eb83f35eaf6a1ec680df2f848e6b4d
subhashinimayrya <subhashini20@navgurukul.org>
2023-03-09 17:43:08 +0530

    changes done

--- backend/constants/generateToken.js
+++ backend/constants/generateToken.js
@@ -1,8 +1,8 @@
 var jwt = require("jsonwebtoken");
 
 function generateAccessToken(data) {
-  let token = jwt.sign(data, process.env.TOKEN_SECRET,{expiresIn:"5sec"});
-    return {token:token,expiry:"5sec"}
+  let token = jwt.sign(data, process.env.TOKEN_SECRET,{expiresIn:"5s"});
+    return {token:token,expiry:"5s"}
   }
 
 module.exports = generateAccessToken;
