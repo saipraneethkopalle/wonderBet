b30a93b49a2dc1f1adb294736e6ffe76acfcf185
Achal123Eminence <achal.mudgal@eminenceinnovation.com>
2023-03-13 10:07:41 +0530

    liveMarketBetComponentChngs

--- frontEndBet/src/app/livemarketbet/livemarketbet.component.ts
+++ frontEndBet/src/app/livemarketbet/livemarketbet.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { tick } from '@angular/core/testing';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiServicesService } from '../api-services.service';
 
 @Component({
   selector: 'app-livemarketbet',
@@ -6,10 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./livemarketbet.component.css']
 })
 export class LivemarketbetComponent implements OnInit {
-
-  constructor() { }
+  matchesAllData:any;
+  sport:any;
+  match:any;
+  constructor(private apiService:ApiServicesService) { }
 
   ngOnInit(): void {
+    document.body.style.backgroundColor="#f0ece1";
+    this.getMatchData();
   }
 
+
+  getMatchData(){
+    this.apiService.getAllMatches().subscribe((res:any)=>{
+      this.matchesAllData = res.data;
+      console.log(res);
+    })
+  }
+
+  getMatchBySport(name:any){
+    if(name.target.value){
+      this.sport = name.target.value;
+    }
+
+    if (this.sport === "All") {
+      // If user selects "All", show all matches
+      this.getMatchData();
+    } else {
+      // If user selects a specific sport, filter the matches by sportId
+      this.apiService.getAllMatches().subscribe((res:any)=>{
+        this.matchesAllData = res.data.filter((data:any) => data.sportId == this.sport);
+      });
+    }
+  }
+  
+  getMatch(val:any){
+    if(val.target.value){
+      this.match = val.target.value;
+    }
+
+    if (this.match === "mAll"){
+      console.log(this.matchesAllData);
+      // If user selects "All", show all matches
+      this.getMatchData();
+    } else {
+      // If user selects a specific sport, filter the matches by sportId
+      this.apiService.getAllMatches().subscribe((res:any)=>{
+        this.matchesAllData = res.data.filter((data:any) => data.eventName == this.match);
+      });
+    }
+  }
 }
+
